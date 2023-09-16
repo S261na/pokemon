689aae6dea3da621ca7cd3ae6ce710fd05b38f95
S261na <sabinastalbekova258@gmail.com>
2023-09-16 21:19:22 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Pokemons from "./components/Pokemon/Pokemons";
-import PokemonCard from "./components/PokemonCard/PokemonCard";
 
 const App = () => {
   return (
     <div>
       <Pokemons />
-      <PokemonCard />
+      
     </div>
   );
 };
--- src/components/Pokemon/Pokemons.jsx
+++ src/components/Pokemon/Pokemons.jsx
@@ -1,18 +1,46 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import PokemonCard from "../PokemonCard/PokemonCard";
+import './Pokemons.css'
 
 const Pokemons = () => {
   const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
-      console.log(res);
-      setPokemon(res?.data);
-    });
+    const pokemonShow = () => {
+      axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) => {
+        const results = response.data.results;
+
+        const pokImg = [];
+        results.map((pokemon) => {
+          axios.get(pokemon.url).then((res) => {
+            pokImg.push(res.data);
+
+            
+            if (pokImg.length === results.length) {
+              setPokemon(pokImg);
+            }
+          });
+        });
+      });
+    };
+
+    pokemonShow();
   }, []);
 
-  return <div>{pokemon && <PokemonCard pokemon={pokemon} />}</div>;
+  return (
+    <div>
+      <h1>POKEMONS</h1>
+      <div className="pok">
+        {pokemon.map((item, index) => (
+          <div key={index} className="Pokemon">
+            <p>{item.name}</p>
+            <img src={item.sprites.front_default} alt="pokemon" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Pokemons;
+
